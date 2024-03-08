c96f7557957660be4db4c37560cc325057785f1d
Oltikocmezi <147403060+Oltikocmezi@users.noreply.github.com>
2024-03-08 16:09:55 +0100

    Changes
    Installed Native libraries, like NavigationContainer, stackNavigator and react native gesture handler

---
+++ NRealEstate/src/consts/houses.jsx
@@ -0,0 +1,55 @@
+const houses = [
+  {
+    id: "1",
+    title: "Entire guest suite",
+    location: "East Side Cedar Cottage Toronto",
+    image: require("../../assets/House1.jpg"),
+    details: "This building is located in the Oliver area",
+    interiors: [
+      require("../../assets/Interior1.jpg"),
+      require("../../assets/Interior2.jpg"),
+      require("../../assets/Interior3.jpg"),
+    ],
+  },
+
+  {
+    id: "2",
+    title: "Entire guest suite",
+    location: "East Side Cedar Cottage Toronto",
+    image: require("../../assets/House2.jpg"),
+    details: "This building is located in the Oliver area",
+    interiors: [
+      require("../../assets/Interior1.jpg"),
+      require("../../assets/Interior2.jpg"),
+      require("../../assets/Interior3.jpg"),
+    ],
+  },
+
+  {
+    id: "3",
+    title: "Entire guest suite",
+    location: "East Side Cedar Cottage Toronto",
+    image: require("../../assets/House3.jpg"),
+    details: "This building is located in the Oliver area",
+    interiors: [
+      require("../../assets/Interior1.jpg"),
+      require("../../assets/Interior2.jpg"),
+      require("../../assets/Interior3.jpg"),
+    ],
+  },
+
+  {
+    id: "4",
+    title: "Entire guest suite",
+    location: "East Side Cedar Cottage Toronto",
+    image: require("../../assets/House4.jpg"),
+    details: "This building is located in the Oliver area",
+    interiors: [
+      require("../../assets/Interior1.jpg"),
+      require("../../assets/Interior2.jpg"),
+      require("../../assets/Interior3.jpg"),
+    ],
+  },
+];
+
+export default houses;
